8b463ff4da29cbb20c0627d91aeec386c39ffda6
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-03-07 19:38:44 -0300

    main window

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,7 +1,10 @@
 package com.project;
 
+import com.project.windows.mainWindow.MainWindow;
+
 public class App {
     public static void main(String[] args) {
-
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
     }
 }
---
+++ src/main/java/com/project/windows/mainWindow/MainWindow.java
@@ -0,0 +1,12 @@
+package com.project.windows.mainWindow;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow () {
+        setSize(320, 500);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Calculator");
+    }
+}
