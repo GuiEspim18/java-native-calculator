b4a4fc6efd571c1e73f407865e60f3ee96a8c4ad
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-03-07 21:22:37 -0300

    add first button

---
+++ src/main/java/com/project/components/buttons/smallButton/SmallButton.java
@@ -0,0 +1,12 @@
+package com.project.components.buttons.smallButton;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SmallButton extends JButton {
+
+    public SmallButton() {
+        setPreferredSize(new Dimension(50, 50));
+    }
+
+}
--- src/main/java/com/project/windows/mainWindow/MainWindow.java
+++ src/main/java/com/project/windows/mainWindow/MainWindow.java
@@ -1,6 +1,9 @@
 package com.project.windows.mainWindow;
 
+import com.project.components.buttons.smallButton.SmallButton;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
     public MainWindow () {
@@ -8,5 +11,11 @@ public class MainWindow extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Calculator");
+
+
+        SmallButton smallButton = new SmallButton();
+        setLayout(new FlowLayout());
+
+        add(smallButton);
     }
 }
