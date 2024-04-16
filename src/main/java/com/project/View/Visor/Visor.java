2b764b2a80fb764fa5f3c822ed1a0ba53470b823
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-04-16 11:29:40 -0300

    visor

---
+++ src/main/java/com/project/View/Visor/Visor.java
@@ -0,0 +1,15 @@
+package com.project.View.Visor;
+
+import com.project.Model.utils.colors.Colors;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Visor extends JPanel {
+
+    public Visor() {
+        setPreferredSize(new Dimension(0, 90));
+        setBackground(Colors.LIGHT_GREY);
+    }
+
+}
--- src/main/java/com/project/View/mainWindow/MainWindow.java
+++ src/main/java/com/project/View/mainWindow/MainWindow.java
@@ -1,6 +1,8 @@
 package com.project.View.mainWindow;
 
+import com.project.Model.utils.colors.Colors;
 import com.project.View.ButtonsHolder.ButtonsHolder;
+import com.project.View.Visor.Visor;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,7 +14,11 @@ public class MainWindow extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Calculator");
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
+        setBackground(Colors.BACKGROUND);
+
+        Visor visor = new Visor();
+        add(visor, BorderLayout.NORTH);
 
         ButtonsHolder buttons = new ButtonsHolder();
         add(buttons);
